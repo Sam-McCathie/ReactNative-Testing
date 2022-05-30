83366e3160d0f005b8bc277a459a3f66d7034502
Sam McCathie <77808434+Sam-McCathie@users.noreply.github.com>
2022-05-30 16:29:19 +1200

    Customised Text input

--- components/UserInputs.tsx
+++ components/UserInputs.tsx
@@ -3,11 +3,13 @@ import {View, Text, TextInput, StyleSheet} from 'react-native';
 
 const UserInputs: React.FC = () => {
   const [name, setName] = useState<string>('');
-  const [mobile, setMobile] = useState<string>();
+  const [mobile, setMobile] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
 
   return (
     <View>
       <Text style={{marginBottom: 10}}>User Inputs</Text>
+
       <Text>Please write your name:</Text>
       <TextInput
         style={styles.textInput}
@@ -15,15 +17,26 @@ const UserInputs: React.FC = () => {
         onChangeText={value => setName(value)}
       />
       <Text style={{marginBottom: 20}}>Name: {name}</Text>
+
       <Text>Please add your mobile:</Text>
       <TextInput
         style={styles.textInput}
         placeholder="021234567"
-        keyboardType="numeric" // when opened on a device -> will open a keyboard with just numbers
+        keyboardType="number-pad" // when opened on a device -> will open a keyboard with just numbers
+        maxLength={10} // max characters
         onChangeText={value => setMobile(value.replace(/[^0-9]/g, ''))} // will replace any value
+        // editable={false} will disable the text field
         value={mobile}
       />
-      <Text>Mobile: {mobile}</Text>
+      <Text style={{marginBottom: 20}}>Mobile: {mobile}</Text>
+
+      <Text>Password (will display below)</Text>
+      <TextInput
+        style={styles.textInput}
+        secureTextEntry
+        onChangeText={value => setPassword(value)}
+      />
+      <Text>Password: {password}</Text>
     </View>
   );
 };
